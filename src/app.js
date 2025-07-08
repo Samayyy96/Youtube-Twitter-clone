fd0461c5b1d7ae584ec1069f747977bee261198b
Samayyy96 <samaytoradmal@gmail.com>
2025-07-09 01:39:53 +0530

    minor changes

--- src/app.js
+++ src/app.js
@@ -5,7 +5,8 @@ import cors from "cors"
 const app = express()
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
 }))
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
